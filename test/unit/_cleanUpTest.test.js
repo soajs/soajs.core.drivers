7b6060dc26fab64c607c4a8730f44524096c028e
nicolas@soajs.org <Ninobaros4>
2017-02-10 18:57:41 +0200

    Removed kubernetes test cases

--- test/unit/_cleanUpTest.test.js
+++ test/unit/_cleanUpTest.test.js
@@ -9,7 +9,7 @@ describe("Clean up kubneretes and docker deployments", function () {
     });
 
     //Remove existing kubernetes deployments
-    it("Cleanup kubernetes deployments", function (done) {
+    it.skip("Cleanup kubernetes deployments", function (done) {
         shell.exec("kubectl delete daemonsets --all --now && kubectl delete deployments --all --now && kubectl delete services --all --now && kubectl delete rs --all --now && kubectl delete pods --all --now");
         done();
     });
