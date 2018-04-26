72e1f0b7d0892732fede2e2498d310eb4b08e01d
mikehajj <mike.hajj@gmail.com>
2018-04-26 20:31:27 +0300

    migrated aws driver from bridge, first iteration.

---
+++ infra/aws/cluster/config.js
@@ -0,0 +1,10 @@
+"use strict";
+
+var config = {
+	"templateUrl": "https://s3.amazonaws.com/herrontech/Templates/Docker-stg.tmpl",
+	"api":{
+		"region": "us-east-1"
+	}
+};
+
+module.exports = config;
