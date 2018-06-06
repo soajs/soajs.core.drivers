47c79d9bef378993bc85b50b765e365b99700db4
RaghebAD <ragheb.abo.dargham@gmail.com>
2018-06-06 16:55:01 +0300

    added skeleton for test cases

---
+++ test/unit/infra/azure/index.unit.test.js
@@ -0,0 +1,164 @@
+"use strict";
+const helper = require("../../../helper");
+const assert = require("assert");
+const sinon = require('sinon');
+
+const service = helper.requireModule('./infra/azure/index.js');
+const infraUtils = helper.requireModule("./infra/utils");
+const azureApi = require('ms-rest-azure');
+
+let dD = require('../../../schemas/azure/cluster.js');
+
+describe("testing /lib/azure/index.js", function () {
+	process.env.SOAJS_CLOOSTRO_TEST = true;
+	
+	describe("calling authenticate", function () {
+		afterEach((done) => {
+			sinon.restore();
+			done();
+		});
+		it("Success", function (done) {
+			done();
+		});
+	});
+	
+	describe("calling getExtras", function () {
+		afterEach((done) => {
+			sinon.restore();
+			done();
+		});
+		it("Success", function (done) {
+			done();
+		});
+	});
+	
+	describe("calling getRegions", function () {
+		afterEach((done) => {
+			sinon.restore();
+			done();
+		});
+		it("Success", function (done) {
+			done();
+		});
+	});
+	
+	describe("calling deployCluster", function () {
+		afterEach((done) => {
+			sinon.restore();
+			done();
+		});
+		it("Success", function (done) {
+			done();
+		});
+	});
+	
+	describe("calling getDeployClusterStatus", function () {
+		afterEach((done) => {
+			sinon.restore();
+			done();
+		});
+		it("Success", function (done) {
+			done();
+		});
+	});
+	
+	describe("calling getDNSInfo", function () {
+		afterEach((done) => {
+			sinon.restore();
+			done();
+		});
+		it("Success", function (done) {
+			done();
+		});
+	});
+	
+	describe("calling scaleCluster", function () {
+		afterEach((done) => {
+			sinon.restore();
+			done();
+		});
+		it("Success", function (done) {
+			done();
+		});
+	});
+	
+	describe("calling getCluster", function () {
+		afterEach((done) => {
+			sinon.restore();
+			done();
+		});
+		it("Success", function (done) {
+			done();
+		});
+	});
+	
+	describe("calling updateCluster", function () {
+		afterEach((done) => {
+			sinon.restore();
+			done();
+		});
+		it("Success", function (done) {
+			done();
+		});
+	});
+	
+	describe("calling deleteCluster", function () {
+		afterEach((done) => {
+			sinon.restore();
+			done();
+		});
+		it("Success", function (done) {
+			done();
+		});
+	});
+	
+	describe("calling publishPorts", function () {
+		afterEach((done) => {
+			sinon.restore();
+			done();
+		});
+		it("Success", function (done) {
+			done();
+		});
+	});
+	
+	describe("calling deployExternalLb", function () {
+		afterEach((done) => {
+			sinon.restore();
+			done();
+		});
+		it("Success", function (done) {
+			done();
+		});
+	});
+	
+	describe("calling updateExternalLB", function () {
+		afterEach((done) => {
+			sinon.restore();
+			done();
+		});
+		it("Success", function (done) {
+			done();
+		});
+	});
+	
+	describe("calling deleteExternalLB", function () {
+		afterEach((done) => {
+			sinon.restore();
+			done();
+		});
+		it("Success", function (done) {
+			done();
+		});
+	});
+	
+	describe("calling executeDriver", function () {
+		afterEach((done) => {
+			sinon.restore();
+			done();
+		});
+		it("Success", function (done) {
+			done();
+		});
+	});
+});
