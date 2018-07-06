2a61694796ebad3df230456221292d88877b565e
RayyanFayyad <rayyan@soajs.com>
2018-07-06 13:09:15 +0300

    group delete and create

--- infra/azure/vm/groups.js
+++ infra/azure/vm/groups.js
@@ -13,7 +13,25 @@ const groups = {
 	* @return {void}
 	*/
     listGroups: function(options, cb) {
-
+      options.soajs.log.debug(`Listing available resource groups`);
+      driverUtils.authenticate(options, (error, authData) => {
+        utils.checkError(error, 700, cb, () => {
+          const resourceClient = driverUtils.getConnector({
+            api: 'resource',
+            credentials: authData.credentials,
+            subscriptionId: options.infra.api.subscriptionId
+          });
+          resourceClient.resourceGroups.list(function (error, resourceGroups) {
+            utils.checkError(error, 714, cb, () => {
+              async.map(resourceGroups, function(oneResourceGroup, callback) {
+                return callback(null, helper.buildResourceGroupRecord({ resourceGroup: oneResourceGroup }));
+              }, function(error, resourceGroupsList) {
+                return cb(null, resourceGroupsList);
+              });
+            });
+          });
+        });
+      });
     },
 
     /**
@@ -24,7 +42,23 @@ const groups = {
 	* @return {void}
 	*/
     createGroup: function(options, cb) {
-
+      options.soajs.log.debug(`Listing available resource groups`);
+      driverUtils.authenticate(options, (error, authData) => {
+        utils.checkError(error, 700, cb, () => {
+          const resourceClient = driverUtils.getConnector({
+            api: 'resource',
+            credentials: authData.credentials,
+            subscriptionId: options.infra.api.subscriptionId
+          });
+          resourceClient.resourceGroups.createOrUpdate(options.params.name, params, options, function (error, response) {
+            utils.checkError(error, 753, cb, () => {
+            function(error, response) {
+                return cb(null, response);
+              });
+            });
+          });
+        });
+      });
     },
 
     /**
@@ -39,13 +73,28 @@ const groups = {
     },
 
     /**
-	* Delete a resource group 
+	* Delete a resource group
 
 	* @param  {Object}   options  Data passed to function as params
 	* @param  {Function} cb    Callback function
 	* @return {void}
 	*/
     deleteGroup: function(options, cb) {
+      options.soajs.log.debug(`Deleting resource group ${options.params.group}`);
+      driverUtils.authenticate(options, (error, authData) => {
+        utils.checkError(error, 700, cb, () => {
+          const resourceClient = driverUtils.getConnector({
+            api: 'resource',
+            credentials: authData.credentials,
+            subscriptionId: options.infra.api.subscriptionId
+          });
+          resourceClient.resourceGroups.deleteMethod(options.params.group, function (error, result) {
+            utils.checkError(error, 708, cb, () => {
+              return cb(null, result);
+            });
+          });
+        });
+      });
 
     }
 
