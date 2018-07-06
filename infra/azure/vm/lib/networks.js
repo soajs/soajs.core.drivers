eb5be4b072a40394158fa41fe6811b9039e08cc9
georgesHayek <georges.hayek@lau.edu>
2018-07-06 15:22:49 +0300

    crud networks

--- infra/azure/vm/lib/networks.js
+++ infra/azure/vm/lib/networks.js
@@ -15,7 +15,25 @@ const networks = {
 	* @return {void}
 	*/
     listNetworks: function(options, cb) {
-
+		options.soajs.log.debug(`Listing Networks for resourcegroup ${options.params.group} `);
+		driverUtils.authenticate(options, (error, authData) => {
+			utils.checkError(error, 700, cb, () => {
+				const networkClient = driverUtils.getConnector({
+					api: 'network',
+					credentials: authData.credentials,
+					subscriptionId: options.infra.api.subscriptionId
+				});
+				networkClient.virtualNetworks.list(options.params.group, function (error, networks) {
+					utils.checkError(error, 731, cb, () => {
+						async.map(networks, function(oneNetwork, callback) {
+							return callback(null, helper.buildNetworkRecord({ network: oneNetwork }));
+						}, function(error, networksList) {
+							return cb(null, networksList);
+						});
+					});
+				});
+			});
+		});
     },
 
     /**
@@ -26,7 +44,21 @@ const networks = {
 	* @return {void}
 	*/
     createNetwork: function(options, cb) {
-
+		options.soajs.log.debug(`Creating network ${options.params.networkName}`);
+		driverUtils.authenticate(options, (error, authData) => {
+			utils.checkError(error, 700, cb, () => {
+				const resourceClient = driverUtils.getConnector({
+					api: 'network',
+					credentials: authData.credentials,
+					subscriptionId: options.infra.api.subscriptionId
+				});
+			resourceClient.virtualNetworks.createOrUpdate(options.params.networkName, params, options, function (error, result) {
+					utils.checkError(error, 747, cb, () => {
+						return cb(null, result);
+					});
+				});
+			});
+		});
     },
 
     /**
@@ -37,7 +69,21 @@ const networks = {
 	* @return {void}
 	*/
     updateNetwork: function(options, cb) {
-
+		options.soajs.log.debug(`Updating network ${options.params.networkName} `);
+		driverUtils.authenticate(options, (error, authData) => {
+			utils.checkError(error, 700, cb, () => {
+				const resourceClient = driverUtils.getConnector({
+					api: 'network',
+					credentials: authData.credentials,
+					subscriptionId: options.infra.api.subscriptionId
+				});
+			resourceClient.virtualNetworks.createOrUpdate(options.params.networkName, params, options, function (error, result) {
+					utils.checkError(error, 748, cb, () => {
+						return cb(null, result);
+					});
+				});
+			});
+		});
     },
 
     /**
@@ -48,9 +94,20 @@ const networks = {
 	* @return {void}
 	*/
     deleteNetwork: function(options, cb) {
-
-    }
-
-};
-
-module.exports = networks;
+		options.soajs.log.debug(`Deleting network ${options.params.networkName}`);
+		driverUtils.authenticate(options, (error, authData) => {
+			utils.checkError(error, 700, cb, () => {
+				const resourceClient = driverUtils.getConnector({
+					api: 'network',
+					credentials: authData.credentials,
+					subscriptionId: options.infra.api.subscriptionId
+				});
+			resourceClient.virtualNetworks.deleteMethod(options.params.group, options.params.networkName, function (error, result) {
+					utils.checkError(error, 742, cb, () => {
+						return cb(null, result);
+					});
+				});
+			});
+		});
+	},
+}
