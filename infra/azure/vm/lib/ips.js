738c35fcfd6a87d7873bc8246c914666a0c88c11
RayyanFayyad <rayyan@soajs.com>
2018-07-06 15:20:42 +0300

    resource Group functions delete functions

--- infra/azure/vm/lib/ips.js
+++ infra/azure/vm/lib/ips.js
@@ -8,66 +8,80 @@ const driverUtils = require('../../utils/index.js');
 const ips = {
 
     /**
-	* List available ip addresses
+    * List available ip addresses
 
-	* @param  {Object}   options  Data passed to function as params
-	* @param  {Function} cb    Callback function
-	* @return {void}
-	*/
+    * @param  {Object}   options  Data passed to function as params
+    * @param  {Function} cb    Callback function
+    * @return {void}
+    */
     list: function(options, cb) {
         options.soajs.log.debug(`Listing public ips for resourcegroup ${options.params.group} `);
-		driverUtils.authenticate(options, (error, authData) => {
-			utils.checkError(error, 700, cb, () => {
-				const networkClient = driverUtils.getConnector({
-					api: 'network',
-					credentials: authData.credentials,
-					subscriptionId: options.infra.api.subscriptionId
-				});
-				networkClient.publicIPAddresses.list(options.params.group,function (error, publicIPAddresses) {
-					utils.checkError(error, 735, cb, () => {
+        driverUtils.authenticate(options, (error, authData) => {
+            utils.checkError(error, 700, cb, () => {
+                const networkClient = driverUtils.getConnector({
+                    api: 'network',
+                    credentials: authData.credentials,
+                    subscriptionId: options.infra.api.subscriptionId
+                });
+                networkClient.publicIPAddresses.list(options.params.group,function (error, publicIPAddresses) {
+                    utils.checkError(error, 735, cb, () => {
 
-						async.map(publicIPAddresses, function(onepublicIPAddresse, callback) {
-							return callback(null, helper.buildPublicIPsRecord({ publicIPAddresse: onepublicIPAddresse }));
-						}, function(error, PublicIpsList) {
-							return cb(null, PublicIpsList);
-						});
-					});
-				});
-			});
-		});
+                        async.map(publicIPAddresses, function(onepublicIPAddresse, callback) {
+                            return callback(null, helper.buildPublicIPsRecord({ publicIPAddresse: onepublicIPAddresse }));
+                        }, function(error, PublicIpsList) {
+                            return cb(null, PublicIpsList);
+                        });
+                    });
+                });
+            });
+        });
     },
 
     /**
-	* Create a new ip address
+    * Create a new ip address
 
-	* @param  {Object}   options  Data passed to function as params
-	* @param  {Function} cb    Callback function
-	* @return {void}
-	*/
+    * @param  {Object}   options  Data passed to function as params
+    * @param  {Function} cb    Callback function
+    * @return {void}
+    */
     create: function(options, cb) {
 
     },
 
     /**
-	* Update an ip address
+    * Update an ip address
 
-	* @param  {Object}   options  Data passed to function as params
-	* @param  {Function} cb    Callback function
-	* @return {void}
-	*/
+    * @param  {Object}   options  Data passed to function as params
+    * @param  {Function} cb    Callback function
+    * @return {void}
+    */
     update: function(options, cb) {
 
     },
 
     /**
-	* Delete an ip address
+    * Delete an ip address
 
-	* @param  {Object}   options  Data passed to function as params
-	* @param  {Function} cb    Callback function
-	* @return {void}
-	*/
+    * @param  {Object}   options  Data passed to function as params
+    * @param  {Function} cb    Callback function
+    * @return {void}
+    */
     delete: function(options, cb) {
-
+        options.soajs.log.debug(`Deleting Public IP ${options.params.publicIp}`);
+        driverUtils.authenticate(options, (error, authData) => {
+            utils.checkError(error, 700, cb, () => {
+                const resourceClient = driverUtils.getConnector({
+                    api: 'network',
+                    credentials: authData.credentials,
+                    subscriptionId: options.infra.api.subscriptionId
+                });
+                resourceClient.publicIPAddresses.deleteMethod(options.params.group, options.params.publicIp, function (error, response) {
+                    utils.checkError(error, 743, cb, () => {
+                        return cb(null, true);
+                    });
+                });
+            });
+        });
     }
 
 };
--- infra/azure/vm/lib/loadBalancers.js
+++ infra/azure/vm/lib/loadBalancers.js
@@ -8,66 +8,80 @@ const driverUtils = require('../../utils/index.js');
 const lbs = {
 
     /**
-	* List available load balancers
+    * List available load balancers
 
-	* @param  {Object}   options  Data passed to function as params
-	* @param  {Function} cb    Callback function
-	* @return {void}
-	*/
+    * @param  {Object}   options  Data passed to function as params
+    * @param  {Function} cb    Callback function
+    * @return {void}
+    */
     list: function(options, cb) {
         options.soajs.log.debug(`Listing laod balancers for resourcegroup ${options.params.group}`);
-		driverUtils.authenticate(options, (error, authData) => {
-			utils.checkError(error, 700, cb, () => {
-				const networkClient = driverUtils.getConnector({
-					api: 'network',
-					credentials: authData.credentials,
-					subscriptionId: options.infra.api.subscriptionId
-				});
-				networkClient.loadBalancers.list(options.params.group, function (error, loadBalancers) {
-					utils.checkError(error, 732, cb, () => {
-						async.map(loadBalancers, function(oneloadBalancer, callback) {
-							return callback(null, helper.buildLoadBalancerRecord({ loadBalancer: oneloadBalancer }));
-						}, function(error, loadBalancersList) {
-							return cb(null, loadBalancersList);
-						});
+        driverUtils.authenticate(options, (error, authData) => {
+            utils.checkError(error, 700, cb, () => {
+                const networkClient = driverUtils.getConnector({
+                    api: 'network',
+                    credentials: authData.credentials,
+                    subscriptionId: options.infra.api.subscriptionId
+                });
+                networkClient.loadBalancers.list(options.params.group, function (error, loadBalancers) {
+                    utils.checkError(error, 732, cb, () => {
+                        async.map(loadBalancers, function(oneloadBalancer, callback) {
+                            return callback(null, helper.buildLoadBalancerRecord({ loadBalancer: oneloadBalancer }));
+                        }, function(error, loadBalancersList) {
+                            return cb(null, loadBalancersList);
+                        });
 
-					});
-				});
-			});
-		});
+                    });
+                });
+            });
+        });
     },
 
     /**
-	* Create a new load balancer
+    * Create a new load balancer
 
-	* @param  {Object}   options  Data passed to function as params
-	* @param  {Function} cb    Callback function
-	* @return {void}
-	*/
+    * @param  {Object}   options  Data passed to function as params
+    * @param  {Function} cb    Callback function
+    * @return {void}
+    */
     create: function(options, cb) {
 
     },
 
     /**
-	* Update a load balancer
+    * Update a load balancer
 
-	* @param  {Object}   options  Data passed to function as params
-	* @param  {Function} cb    Callback function
-	* @return {void}
-	*/
+    * @param  {Object}   options  Data passed to function as params
+    * @param  {Function} cb    Callback function
+    * @return {void}
+    */
     update: function(options, cb) {
 
     },
 
     /**
-	* Delete a load balancer
+    * Delete a load balancer
 
-	* @param  {Object}   options  Data passed to function as params
-	* @param  {Function} cb    Callback function
-	* @return {void}
-	*/
+    * @param  {Object}   options  Data passed to function as params
+    * @param  {Function} cb    Callback function
+    * @return {void}
+    */
     delete: function(options, cb) {
-
+        options.soajs.log.debug(`Deleting load Balancer ${options.params.lbname}`);
+        driverUtils.authenticate(options, (error, authData) => {
+            utils.checkError(error, 700, cb, () => {
+                const resourceClient = driverUtils.getConnector({
+                    api: 'network',
+                    credentials: authData.credentials,
+                    subscriptionId: options.infra.api.subscriptionId
+                });
+                resourceClient.loadBalancers.deleteMethod(options.params.group, options.params.lbname, function (error, response) {
+                    utils.checkError(error, 741, cb, () => {
+                        return cb(null, true);
+                    });
+                });
+            });
+        });
     }
 
 };
