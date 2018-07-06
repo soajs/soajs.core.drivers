de970b59eba9edaae8d3637c571cf2a482894b1a
Ameer Faraj <faraj.ameer@gmail.com>
2018-07-06 11:42:39 +0300

    Added skeleton for azure vm driver -> groups

---
+++ infra/azure/vm/groups.js
@@ -0,0 +1,54 @@
+'use strict';
+
+const utils = require('../../../lib/utils/utils.js');
+const driverUtils = require('../utils/index.js');
+
+const groups = {
+
+    /**
+	* List available resource groups
+
+	* @param  {Object}   options  Data passed to function as params
+	* @param  {Function} cb    Callback function
+	* @return {void}
+	*/
+    listGroups: function(options, cb) {
+
+    },
+
+    /**
+	* Create a new resource group
+
+	* @param  {Object}   options  Data passed to function as params
+	* @param  {Function} cb    Callback function
+	* @return {void}
+	*/
+    createGroup: function(options, cb) {
+
+    },
+
+    /**
+	* Update a resource group
+
+	* @param  {Object}   options  Data passed to function as params
+	* @param  {Function} cb    Callback function
+	* @return {void}
+	*/
+    updateGroup: function(options, cb) {
+
+    },
+
+    /**
+	* Delete a resource group 
+
+	* @param  {Object}   options  Data passed to function as params
+	* @param  {Function} cb    Callback function
+	* @return {void}
+	*/
+    deleteGroup: function(options, cb) {
+
+    }
+
+};
+
+module.exports = groups;
