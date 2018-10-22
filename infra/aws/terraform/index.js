56679aba63d94d01941296c589ecd3aab3ab4f80
mikehajj <mike.hajj@gmail.com>
2018-10-22 13:58:51 +0300

    added support to map vpcid from network name when creating and modifying vm clusters.

--- infra/aws/terraform/index.js
+++ infra/aws/terraform/index.js
@@ -2,6 +2,7 @@
 
 const terraform = require(`${__dirname}/../../../lib/terraform`);
 const utils = require('../../../lib/utils/utils.js');
+const network = require("../cluster/networks");
 
 const driver = {
 
@@ -20,11 +21,48 @@ const driver = {
         options.params.input.secretKey = options.infra.api.secretAccessKey;
 
         options.params.input.envCode = options.registry.code;
-        terraform.apply(options,function(error, result){
-            utils.checkError(error, 738, cb, () => {
-                return cb(null, result);
-            });
-        });
+        
+        //get the vpc id from the network name
+	    getNetworkIdFromName(options, (error, networkToUse) => {
+		    if (error) {
+			    return cb(error);
+		    }
+		
+		    options.params.input.networkId = networkToUse.id;
+			    
+	        terraform.apply(options,function(error, result){
+	            utils.checkError(error, 738, cb, () => {
+	                return cb(null, result);
+	            });
+	        });
+	    });
+	
+	    function getNetworkIdFromName(options, cb){
+		    let originalinputs = JSON.parse(JSON.stringify(options.params.input));
+		    options.params.region = options.params.input.region;
+		    options.params.network = options.params.input.network;
+		    network.list(options, (error, response) => {
+			    if(error){
+				    return cb(error);
+			    }
+			    options.params.input = originalinputs;
+			    delete options.params.network;
+			    delete options.params.region;
+			
+			    let data;
+		     
+			    response.forEach((oneNetwork) => {
+				    if(oneNetwork.name === options.params.input.network){
+					    data = oneNetwork;
+				    }
+				    else if(oneNetwork.id === options.params.input.network){
+					    data = oneNetwork;
+				    }
+			    });
+			
+			    return cb(null, data);
+		    });
+	    }
     },
 
     /**
